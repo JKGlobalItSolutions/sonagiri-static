2c43b1d4ed3a0abd161884b7e484534993b4233e
sriram A <sriram.coder2004@gmail.com>
2025-06-18 18:07:03 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/sonagiri_static/',   // <--- important: match your repo name
+  base: '/sonagiri-static/',   // <--- important: match your repo name
   plugins: [react()],
 })
